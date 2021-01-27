c378dae26ef6410a0ad1c90085935d6b8dcc01eb
gyumin-kim <starkying@gmail.com>
2021-01-27 15:12:52 +0900

    feat: 실시간 쇼핑 검색어

--- src/components/Billboard.vue
+++ src/components/Billboard.vue
@@ -1,13 +1,13 @@
 <template>
-  <div 
+  <div
     :style="`background-color: ${currentColor};`"
     class="billboard">
     <div class="inner">
-      <div 
+      <div
         ref="swiper"
         class="swiper-container">
         <div class="swiper-wrapper">
-          <div 
+          <div
             v-for="item in billboards"
             :key="item.name"
             class="swiper-slide">
@@ -42,192 +42,191 @@
     </div>
   </div>
 </template>
-
-
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from 'vuex'
 import Swiper from 'swiper/bundle'
 import 'swiper/swiper-bundle.css'
 
 export default {
-    data() {
-        return {
-            billboards: [],
-            currentIndex: 0,
-            currentColor: '',
-            isAutoplay: false,
-            swiper: null,
-            done: false
-        }
-    },
-    mounted () {
-        this.init()
-    },
-    methods: {
-        ...mapActions('fetchData', [
-            'fetchData'
-        ]),
-        async init() {
-            this.billboards = await this.$fetch({
-                requestName: 'billboards'
-            })
-            this.$nextTick(() => {
-                this.swiper = new Swiper(this.$refs.swiper, {
-                    effect: 'fade',
-                    speed: 1000,
-                    autoplay: {
-                        delay: 3000
-                    },
-                    loop: true,
-                    preloadImages: false,
-                    lazy: {
-                        loadPrevNext: true,
-                        loadPrevNextAmount: 2
-                    },
-                    navigation: {
-                        nextEl: '.billboard .next',
-                        prevEl: '.billboard .prev'
-                    },
-                    on: {
-                        slideChange: swiper => {
-                            const { realIndex } = swiper
-                            this.currentIndex = realIndex
-                            this.currentColor = this.billboards[realIndex].color
-                        },
-                        autoplayStart: () => {
-                            this.isAutoplay = true
-                        },
-                        autoplayStop: () => {
-                            this.isAutoplay = false
-                        }
-                    }
-                })
-            })
-        },
-        toggleAutoplay() {
-            if (this.isAutoplay) {
-                this.swiper.autoplay.stop()
-            } else {
-                this.swiper.autoplay.start()
-            }
-        }
+  data () {
+    return {
+      billboards: [],
+      swiper: null,
+      isAutoplay: false,
+      currentIndex: 0,
+      currentColor: '',
+      done: false
     }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    ...mapActions('fetchData', [
+      'fetchData'
+    ]),
+    async init () {
+      this.billboards = await this.$fetch({
+        requestName: 'billboards'
+      })
+      this.$nextTick(() => {
+        // https://swiperjs.com/api/
+        this.swiper = new Swiper(this.$refs.swiper, {
+          effect: 'fade',
+          speed: 1000,
+          autoplay: {
+            delay: 3000
+          },
+          loop: true,
+          preloadImages: false,
+          lazy: {
+            loadPrevNext: true,
+            loadPrevNextAmount: 2
+          },
+          navigation: {
+            nextEl: '.billboard .next',
+            prevEl: '.billboard .prev'
+          },
+          on: {
+            slideChange: swiper => {
+              const { realIndex } = swiper
+              this.currentIndex = realIndex
+              this.currentColor = this.billboards[realIndex].color
+            },
+            autoplayStart: () => {
+              this.isAutoplay = true
+            },
+            autoplayStop: () => {
+              this.isAutoplay = false
+            }
+          }
+        })
+      })
+    },
+    toggleAutoplay () {
+      if (this.isAutoplay) {
+        this.swiper.autoplay.stop()
+      } else {
+        this.swiper.autoplay.start()
+      }
+    }
+  }
 }
 </script>
 
 <style scoped lang="scss">
 .billboard {
-    transition: background-color 1s;
-    .inner {
-        width: 1240px;
-        margin: 0 auto;
-        position: relative;
-        .swiper-container {
-            width: 1240px;
-            height: 400px;
-            .swiper-lazy-preloader {
-                border-color: #F43242;
-                border-top-color: transparent;
-            }
-        }
-        .controls {
-            display: flex;
-            position: absolute;
-            bottom: 25px;
-            right: 0;
-            z-index: 1;
-            .controller {
-                background-color: rgba(#000, .2);
-                border-radius: 22.5px;
-                margin-right: 10px;
-                display: flex;
-                &:last-child {
-                    margin-right: 0;
-                }
-                .autoplay {
-                    width: 45px;
-                    height: 45px;
-                    cursor: pointer;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    &.pause {
-                        &::after {
-                            background-position: -170px -29px;
-                        }
-                    }
-                    &::after {
-                        content: "";
-                        display: block;
-                        width: 24px;
-                        height: 24px;
-                        background-image: url("https://trusting-williams-8cacfb.netlify.app/images/main_2x.png");
-                        background-position: -170px -87px;
-                        background-size: 209px;
-                    }
-                }
-                .pagination {
-                    display: flex;
-                    align-items: center;
-                    height: 45px;
-                    padding: 0 4px 0 22px;
-                    color: #ddd;
-                    font-size: 17px;
-                    strong {
-                        color: #fff;
-                        margin-right: 4px;
-                        font-weight: 700;
-                    }
-                }
-                .open-more {
-                    width: 45px;
-                    height: 45px;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    &::after {
-                        content: "";
-                        display: block;
-                        width: 24px;
-                        height: 24px;
-                        background-image: url("https://trusting-williams-8cacfb.netlify.app/images/main_2x.png");
-                        background-position: -170px 0px;
-                        background-size: 209px;
-                    }
-                }
-                .navi {
-                    width: 45px;
-                    height: 45px;
-                    display: flex;
-                    position: relative;
-                    outline: none;                                                                                                      
-                    cursor: pointer;
-                    &::after {
-                        content: "";
-                        position: absolute;
-                        top: 0;
-                        bottom: 0;
-                        margin: auto;
-                        width: 24px;
-                        height: 24px;
-                        background-image: url("https://trusting-williams-8cacfb.netlify.app/images/main_2x.png");
-                        background-size: 209px;
-                    }
-                }
-                .prev {
-                    &::after {
-                        right: 6px;
-                        background-position: -170px -58px;
-                    }
-                }
-                .next {
-                    &::after {
-                        left: 6px;
-                        background-position: -170px -116px;
-                    }
-                }
-            }
-        }
+  transition: background-color 1s;
+  .inner {
+    width: 1240px;
+    margin: 0 auto;
+    position: relative;
+    .swiper-container {
+      width: 1240px;
+      height: 400px;
+      .swiper-lazy-preloader {
+        border-color: #F43242;
+        border-top-color: transparent;
+      }
     }
+    .controls {
+      display: flex;
+      position: absolute;
+      bottom: 25px;
+      right: 0;
+      z-index: 1;
+      .controller {
+        background-color: rgba(#000, .2);
+        border-radius: 22.5px;
+        margin-right: 10px;
+        display: flex;
+        &:last-child {
+          margin-right: 0;
+        }
+        .autoplay {
+          width: 45px;
+          height: 45px;
+          cursor: pointer;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          &.pause {
+            &::after {
+              background-position: -170px -29px;
+            }
+          }
+          &::after {
+            content: "";
+            display: block;
+            width: 24px;
+            height: 24px;
+            background-image: url("https://trusting-williams-8cacfb.netlify.app/images/main_2x.png");
+            background-position: -170px -87px;
+            background-size: 209px;
+          }
+        }
+        .pagination {
+          display: flex;
+          align-items: center;
+          height: 45px;
+          padding: 0 4px 0 22px;
+          color: #ddd;
+          font-size: 17px;
+          strong {
+            color: #fff;
+            margin-right: 4px;
+            font-weight: 700;
+          }
+        }
+        .open-more {
+          width: 45px;
+          height: 45px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          &::after {
+            content: "";
+            display: block;
+            width: 24px;
+            height: 24px;
+            background-image: url("https://trusting-williams-8cacfb.netlify.app/images/main_2x.png");
+            background-position: -170px 0px;
+            background-size: 209px;
+          }
+        }
+        .navi {
+          width: 45px;
+          height: 45px;
+          display: flex;
+          position: relative;
+          outline: none;
+          cursor: pointer;
+          &::after {
+            content: "";
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            margin: auto;
+            width: 24px;
+            height: 24px;
+            background-image: url("https://trusting-williams-8cacfb.netlify.app/images/main_2x.png");
+            background-size: 209px;
+          }
+        }
+        .prev {
+          &::after {
+            right: 6px;
+            background-position: -170px -58px;
+          }
+        }
+        .next {
+          &::after {
+            left: 6px;
+            background-position: -170px -116px;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
